2833c05a899f49245e522df4b0bf66a538173b61
Jennie Zeng <jenniezeng1@gmail.com>
2019-05-08 21:47:41 -0700

    complete palidrome method

--- src/recursion.js
+++ src/recursion.js
@@ -148,6 +148,17 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+    if (string.length === 0 || string.length === 1) {
+        return true;
+    }
+
+    string = string.toLowerCase().trim();
+
+    if (string[0] === string[string.length - 1]) {
+        return palindrome(string.slice(1, string.length - 1));
+    } else {
+        return false;
+    }
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
