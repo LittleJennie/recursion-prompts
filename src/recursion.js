e24c898e94a610e8519072dd782781d8e6105f03
Jennie Zeng <jenniezeng1@gmail.com>
2019-05-09 21:17:23 -0700

    complete gcd method

--- src/recursion.js
+++ src/recursion.js
@@ -294,6 +294,21 @@ var divide = function(x, y) {
 // http://www.cse.wustl.edu/~kjg/cse131/Notes/Recursion/recursion.html
 // https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
 var gcd = function(x, y) {
+    if (x < 0 || y < 0) {
+        return null;
+    }
+    if (x < y) {
+        return gcd(y, x);
+    }
+    if (x === y) {
+        return x;
+    }
+
+    if (y === 0) {
+        return x;
+    } else {
+        return gcd(y, x % y);
+    }
 };
 
 // 15. Write a function that compares each character of two strings and returns true if
